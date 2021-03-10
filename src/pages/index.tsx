995056f9b94f81111aa42b20a563a05946f7a17c
Binner <lucian.binner@hotmail.com>
2021-03-10 09:41:11 -0300

    complements styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../styles/global-styles';
+import { theme } from '../styles/theme';
 
 const IndexPage = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <h1>Hello World!</h1>
     <GlobalStyles />
-  </>
+  </ThemeProvider>
 );
 
 export default IndexPage;
